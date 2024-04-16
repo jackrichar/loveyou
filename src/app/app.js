b687e2129e6061a5f8a2536c20cc6b783d304c08
Sakbari <96303727+jackrichar@users.noreply.github.com>
2024-04-16 18:02:42 +0330

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './appStyle.css'
+import Content from "../component/background";
+const App = () =>{
+    return(
+        <div className="background">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+                <Content/>
+        </div>
+    )
+}
+export default App;
---
+++ src/component/background.jsx
@@ -0,0 +1,70 @@
+import React, {useState} from "react";
+import './backgroundStyle.css'
+import styled from "styled-components";
+import {useEffect} from "react";
+
+const Content = () =>{
+    const [Check,setCheck] = useState(false);
+    const [Count,setCount] = useState(1);
+    const [None,setNone] = useState('');
+    const [Block,setBlock] = useState('');
+    return(
+        <div className={"Content"}>
+            <div className={"Button"} style={{display:`${None}`}}>
+                <div onClick={Animation} style={Check === true ? translateX_right() : translateX_left()} className={"Button-Row"}>
+                    <div className={"Button-Text"}>
+                        <h4>&#128151;کلیک کن</h4>
+                    </div>
+                </div>
+            </div>
+            <div className={"Text"} style={{display:`${Block}`}}>
+                <p className={"Paragraph"}>
+                    هر وقت خستم بهم انرژی میدی
+                    هر وقت دلم گرفته با گفتن کلمه دوست دارم بهم روحیه میدی که نباید تسلیم بشم
+                    نمیدونم قبل تو چطوری زندگی میکردم
+                    و نمیدونم بدون تو هم چطوری میخوام زندگی کنم
+                    اما الان اینو میدونم که هر روز با هدف بیدار میشم چون میدونم یکی هست که به فکرمه و یکی هست که برای رسیدن بهش تلاش کنم
+                    هر چند مشکلات زیادی بین ما هست
+                    اما میتونیم باهم از این مشکلات گذشت کنیم
+                    و همچنان منتظر 31 شهریور روز به دنیا اومدن زیباترین گل هستم
+                    امیدوارم شما هم همین حس رو نسبت به من داشته باشی
+                    باز هم مثل همیشه در جمله آخر باید بگم با تمام این شرایط
+                    دوست دارم.
+                </p>
+            </div>
+        </div>
+    );
+
+    function Animation(){
+        setCount(Count + 1);
+        if(Count === 1){
+            setCheck(true);
+        }else if(Count === 2){
+            setCheck(false);
+            setCount(1);
+        }
+        console.log(Count,Check)
+    }
+    function translateX_right() {
+        ShowingContent();
+        return {
+            transform: 'translateX(117%)',
+            boxShadow: 'inset 0 0 20px 0 black, -9px 0 20px 20px black',
+        };
+    }
+    function translateX_left() {
+        return {
+            transform: 'translateX(0%)',
+            boxShadow: 'inset 0 0 20px 0 black, 9px 0 20px 20px black'
+        };
+    }
+    function ShowingContent(){
+        setTimeout(()=>{
+            console.log("hide");
+            setNone('none');
+            setBlock('block');
+        },300)
+    }
+}
+
+export default Content;
